4a9962af76e663235bff115cf536975b95df03fd
Salavat <salavat2602@mail.ru>
2025-10-16 11:28:18 +0500

    optimize

--- qaqa/src/components/app/App.jsx
+++ qaqa/src/components/app/App.jsx
@@ -1,12 +1,58 @@
-import {useState, memo} from 'react';
-import {Container} from 'react-bootstrap';
+import { useState, memo, useCallback, use } from "react";
+import { Container } from "react-bootstrap";
 
+// class Form extends Component {
+//   shouldComponentUpdate(nextProps) {
+//     if (this.props.mail.name === nextProps.mail.name) {
+//       return false;
+//     } return true;
+//   }
+//   render() {
+//     console.log("render");
+//     return (
+//       <Container>
+//         <form className="w-50 border mt-5 p-3 m-auto">
+//           <div className="mb-3">
+//             <label
+//               htmlFor="exampleFormControlInput1"
+//               className="form-label mt-3"
+//             >
+//               Email address
+//             </label>
+//             <input
+//               value={this.props.mail.name}
+//               type="email"
+//               className="form-control"
+//               id="exampleFormControlInput1"
+//               placeholder="name@example.com"
+//             />
+//           </div>
+//           <div className="mb-3">
+//             <label htmlFor="exampleFormControlTextarea1" className="form-label">
+//               Example textarea
+//             </label>
+//             <textarea
+//               value={this.props.text}
+//               className="form-control"
+//               id="exampleFormControlTextarea1"
+//               rows="3"
+//             ></textarea>
+//           </div>
+//         </form>
+//       </Container>
+//     );
+//   }
+// }
 
 function propsCompare(prevProps, nextProps) {
-    return prevProps.mail === nextProps.mail && prevProps.text === nextProps.text;
+    return prevProps.mail === nextProps.mail ;
 }
 
+
+
 const Form = memo((props) => {
+    console.log('render');
+    
 
     return (
         <Container>
@@ -22,26 +68,34 @@ const Form = memo((props) => {
             </form>
         </Container>
     )
-}, propsCompare)
+});
 
 function App() {
-    const [data, setData] = useState({
-        mail: "name@example.com",
-        text: 'some text'
-    });
+  const [data, setData] = useState({
+    mail: "second@example.com",
+    text: "some text",
+  });
 
-    return (
-        <>
-            <Form mail={data.mail} text={data.text}/>
-            <button  className=''
-                onClick={() => setData({
-                    mail: "second@example.com",
-                    text: 'another text'
-                })}>
-                Click me
-            </button>
-        </>
-    );
+  const onLog = useCallback (() => {
+    console.log("wow");
+  }, [])
+
+  return (
+    <>
+      <Form mail={data.mail} text={data.text} onLog={onLog} />
+      <button
+        className=""
+        onClick={() =>
+          setData({
+            mail: "second@example.com",
+            text: "another text",
+          })
+        }
+      >
+        Click me
+      </button>
+    </>
+  );
 }
 
 export default App;
